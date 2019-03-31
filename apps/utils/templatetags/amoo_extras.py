d461cca65028c13ad2d346cdacd9f09117342a59
saeed <stahmasebi30@gmail.com>
2019-03-31 23:02:38 +0430

    add persian digits filter

--- apps/utils/templatetags/amoo_extras.py
+++ apps/utils/templatetags/amoo_extras.py
@@ -1,5 +1,11 @@
+import re
 from datetime import datetime
+
+from django import template
+
 from apps.utils import jalali
+
+register = template.Library()
 
 JALALI_MONTHS = ['فروردین', 'اردیبهشت', 'خرداد',
                  'تیر', 'مرداد', 'شهریور',
@@ -7,10 +13,18 @@ JALALI_MONTHS = ['فروردین', 'اردیبهشت', 'خرداد',
                  'دی', 'بهمن', 'اسفند', ]
 
 
+@register.filter
 def jalali_date(date):
     if not isinstance(date, datetime):
-        raise Exception('jalali_date templatetag need datetime object')
+        return None
     date = date.strftime('%Y-%m-%d')
     jy, jm, jd = jalali.Gregorian(date).persian_tuple()
     month_name = JALALI_MONTHS[jm - 1]
-    return "{day} {month} {year}".format(day=jd, month=month_name, year=jy)
+    return "{day} {month} {year}".format(day=persian_digits(jd), month=month_name, year=persian_digits(jy))
+
+
+@register.filter
+def persian_digits(s):
+    s = str(s)
+    digits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
+    return re.sub(r'0|1|2|3|4|5|6|7|8|9', lambda c: digits[int(c.group())], s)
