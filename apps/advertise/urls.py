bbb3c7f4c8c1f6a819c7068818cd07735b42867f
saeed <stahmasebi30@gmail.com>
2019-04-07 19:53:19 +0430

    add creating new advertise

--- apps/advertise/urls.py
+++ apps/advertise/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from.views import AdvertiseListView, AdvertiseDetailView
+from . import views
 
 app_name = 'advertise'
 urlpatterns = [
-    url(r'^$', AdvertiseListView.as_view(), name='list'),
-    url(r'^(?P<pk>[0-9]+)/$', AdvertiseDetailView.as_view(), name='detail'),
+    url(r'^$', views.AdvertiseListView.as_view(), name='list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.AdvertiseDetailView.as_view(), name='detail'),
+    url(r'^add/$', views.add_advertise, name='add_advertise'),
 ]
--- apps/advertise/views.py
+++ apps/advertise/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+
 from .models import Advertise
+from .forms import AdvertiseForm
 
 
 class AdvertiseListView(ListView):
@@ -13,3 +18,20 @@ class AdvertiseListView(ListView):
 class AdvertiseDetailView(DetailView):
     template_name = 'advertise/advertise_detail.html'
     model = Advertise
+
+
+def add_advertise(request):
+    if request.method == 'POST':
+        form = AdvertiseForm(request.POST)
+        if form.is_valid():
+            advertise = form.save(commit=False)
+            advertise.user = User.objects.first()
+            advertise.state = Advertise.PENDING
+            advertise.save()
+            messages.add_message(request, messages.SUCCESS, _('Advertise added successfully.'))
+            return redirect('advertise:list')
+        else:
+            messages.add_message(request, messages.ERROR, _('Please correct the following errors.'))
+    else:
+        form = AdvertiseForm()
+    return render(request, 'advertise/new_advertise.html', {'form': form})
